774bcc2d3eea17d991984b4833b2a2760247a92d
DJkitty2 <tobycat133@gmail.com>
2025-03-05 17:36:49 -0500

    i give up

---
+++ keybordtest.py
@@ -0,0 +1,17 @@
+import keyboard
+import pyttsx3
+from voicetotext import transcribe_audio
+from texttoollama import get_llama_response
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)  # Speed of speech (words per minute)
+engine.setProperty('volume', 1.0)  # Volume (0.0 to 1.0)
+
+while True:
+    if keyboard.read_key() == "p":
+        text = transcribe_audio("audio.mp3")
+        response = get_llama_response(text)
+        engine.say(response)  # Concatenate the strings
+        print("Ollama's Response:", response)
+        print("You pressed p")
+        engine.runAndWait()  # This line is necessary to actually produce speech
