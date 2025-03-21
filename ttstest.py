f7355dbacbfc6a760778587c561598597f68763c
DJkitty <DJkittytoby@gmail.com>
2025-03-21 18:28:42 -0400

    sync and they killed my power

--- ttstest.py
+++ ttstest.py
@@ -1,16 +1,3 @@
-import psutil
+import test from sigma
 
-# Get total physical memory in bytes
-total_memory_bytes = psutil.virtual_memory().total
-
-# Convert to gigabytes
-total_memory_gb = total_memory_bytes / (1024 ** 3)
-model = "gemma3:1b"
-
-if total_memory_gb < 10:
-    print("under 10")
-    print("keeping gemma3:1b")
-else:
-    print("over 10")
-    model = "gemma3:12b"
-    print("using gemma3:12b")
+sigma()
