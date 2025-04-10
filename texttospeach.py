fe74c45c9e654f54fba3ee9e953dc008981fe0f7
DJkitty2 <tobycat133@gmail.com>
2025-04-10 16:30:06 -0400

    fixed stuff

--- texttospeach.py
+++ texttospeach.py
@@ -2,7 +2,6 @@ from TTS.api import TTS
 import simpleaudio as sa
 import re
 import os
-from newmain import fun_speed
 
 if os.name == "nt":
     device = "cuda"
@@ -14,14 +13,11 @@ def clean_text(text):
     return re.sub(r'[^\x00-\x7F]+', '', text)  # Keeps only standard ASCII characters
 
 def speak_text(text):
-    speak_speed=fun_speed()
-    print(speak_speed)
     text = clean_text(text)  # Clean text before passing to TTS
     tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True)
     tts.to(device)
     tts.tts_to_file(
         text=text,
-        speed=speak_speed, 
         file_path="output.wav"
         )
     
